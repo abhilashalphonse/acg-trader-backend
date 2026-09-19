f900d0705e6f6314baab5cb597cba6f225b6861a
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-19 01:38:40 +0100

    feat: standardize market candle timeframes

--- src/modules/market-data/market.constants.js
+++ src/modules/market-data/market.constants.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const TIMEFRAME_MS = Object.freeze({
-  '1s': 1_000,
-  '5s': 5_000,
-  '15s': 15_000,
-  '30s': 30_000,
   '1m': 60_000,
   '5m': 5 * 60_000,
   '15m': 15 * 60_000,
+  '30m': 30 * 60_000,
   '1h': 60 * 60_000,
   '4h': 4 * 60 * 60_000,
   '1d': 24 * 60 * 60_000,
+  '1w': 7 * 24 * 60 * 60_000,
 });
 
 const TWELVE_DATA_HISTORY_INTERVALS = Object.freeze({
   '1m': '1min',
   '5m': '5min',
   '15m': '15min',
+  '30m': '30min',
   '1h': '1h',
   '4h': '4h',
   '1d': '1day',
+  '1w': '1week',
 });
 
 const MARKET_CONNECTION_STATES = Object.freeze({
