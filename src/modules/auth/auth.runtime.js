42be596f6eb551897e74aac3c1e308b0331591cd
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-19 01:07:33 +0100

    feat: wire persistent auth session settings

--- src/modules/auth/auth.runtime.js
+++ src/modules/auth/auth.runtime.js
@@ -6,6 +6,9 @@ const { AuthService } = require('./auth.service');
 function createAuthRuntime() {
   const authService = new AuthService({
     sessionTtlSeconds: env.auth.sessionTtlSeconds,
+    accessTokenTtlSeconds: env.auth.accessTokenTtlSeconds,
+    refreshSessionTtlSeconds: env.auth.refreshSessionTtlSeconds,
+    idleTimeoutSeconds: env.auth.idleTimeoutSeconds,
     federationTicketTtlSeconds: env.auth.federationTicketTtlSeconds,
     maxFailedLogins: env.auth.maxFailedLogins,
     lockoutSeconds: env.auth.lockoutSeconds,
