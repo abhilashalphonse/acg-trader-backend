96c24b1eb627a2b4b6557345a8505733a9725602
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-26 21:51:59 +0100

    Advance financial revision on execution mutations

--- src/modules/trading/market-order.service.js
+++ src/modules/trading/market-order.service.js
@@ -551,6 +551,7 @@ class MarketOrderService {
 }
 
 function applyOpenAccountMutation(account, plan) {
+  account.financialRevision = Number(account.financialRevision || 0) + 1;
   const balance = subtractDecimal(account.state.balance, plan.commission);
   const equity = subtractDecimal(account.state.equity, plan.commission);
   const usedMargin = addDecimal(account.state.usedMargin, plan.requiredMargin);
@@ -561,6 +562,7 @@ function applyOpenAccountMutation(account, plan) {
 }
 
 function applyCloseAccountAndPositionMutation({ account, position, plan, deal, clientOrderId, ledgerModel, now, valuationComplete = true, closeReason = 'MANUAL' }) {
+  account.financialRevision = Number(account.financialRevision || 0) + 1;
   const ledgers = [];
   let runningBalance = normalizeDecimal(account.state.balance);
 
